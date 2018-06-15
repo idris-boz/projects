abbb93fb56988acb3c12cbaa801d5a75e7cda1e4
ido <ido@DESKTOP-N8FDACQ>
2018-06-15 11:56:45 +0300

    OTO

---
+++ WindowsFormsApp1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Class1
+    {
+        [NUnit.Framework.Test]
+        public void A()
+        {
+            int x;
+            x = 2;
+        }
+    }
+}
